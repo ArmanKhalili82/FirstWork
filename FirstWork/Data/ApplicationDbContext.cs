28e57d91fea2cb839c812e19c27c26ec23da6aa5
ArmanKhalili82 <hykhalili@yahoo.com>
2023-11-18 22:49:58 +0100

    Add project files.

---
+++ FirstWork/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using FirstWork.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWork.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(@"Server=.;Database=FirstTable;Encrypt=False;Integrated Security=True");
+
+        public DbSet<Table> Tables { get; set; }
+    }
+}
---
+++ FirstWork/Data/Models/Table.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstWork.Data.Models
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public string Class { get; set; }
+        public string Style { get; set; }
+    }
+}
